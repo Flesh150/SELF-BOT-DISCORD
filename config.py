1a3056b5ff21e867aa3ef87143bdbd87667d0b8f
Flesh150 <85175567+Flesh150@users.noreply.github.com>
2022-04-14 20:34:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
+'''
+Создатель: Flesh
+Донаты: https://www.donationalerts.com/r/flesh150
+GitHub: https://github.com/Flesh150
+Канал в тг: https://t.me/flesh_150
+'''
+
 TOKEN = '' #Токен от вашего аккаунта
 PREFIX = '' #Префикс команд
